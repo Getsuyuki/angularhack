22c11e679bb09eeeca207f46b7e05ba015ea0acf
David Li <li.zq.david@gmail.com>
2015-04-20 20:37:02 -0400

    added random files for random project
    soi

---
+++ main.js
@@ -0,0 +1,19 @@
+//Define an angular module for our app
+var es = angular.module('eventies', []);
+ 
+//Define Routing for app
+// es.config(['$routeProvider',
+  // function($routeProvider) {
+    // $routeProvider.
+      // when('/Home', {
+        // templateUrl: 'maininfo.html',
+        // controller: ''
+    // }).
+      // when('', {
+        // templateUrl: '',
+        // controller: ''
+      // })
+// }]);
+// angular.module('eventies').controller('searchControl', ['$scope', '$http', function($scope, $http){
+    // $scope.greeting = 'hi!';
+// }]);
